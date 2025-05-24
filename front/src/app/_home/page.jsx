5a6806a920adcbbd9b704a752227f3e141f6bd66
Fernando Brettas <fernandochbrettas@gmail.com>
2025-05-24 11:21:30 -0300

    Adicionar botao de voltar

--- front/src/app/_home/page.jsx
+++ front/src/app/_home/page.jsx
@@ -1,22 +1,24 @@
+import Link from "next/link";
+
 export default function HomePage() {
     return (
         <div className="max-w-screen-md mx-auto w-full h-screen flex justify-center items-center px-2">
             <div className="bg-white w-full sm:w-2/4 flex flex-col items-center border border-black rounded-xl gap-5 py-7 px-5">
                 <p className="font-bold bg-[#FFB8B8] mb-8 p-2 rounded-2xl">Estou entrando como:</p>
 
-                <a 
-                    href="/loginMedico"
+                <Link 
+                    href={"/loginMedico"}
                     className="w-32 text-center bg-[#FFB8B8] p-2 rounded-2xl"
                 >
                     Médico(a)
-                </a>
+                </Link>
 
-                <a 
-                    href=""
+                <Link 
+                    href={""}
                     className="w-32 text-center bg-[#FFB8B8] p-2 rounded-2xl"
                 >
                     Enfermeiro(a)
-                </a>
+                </Link>
             </div>
         </div>
     )
--- front/src/app/loginMedico/page.jsx
+++ front/src/app/loginMedico/page.jsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
+import { FiArrowLeft } from "react-icons/fi";
+
 export default function LoginMedico() {
     return (
         <div className="max-w-screen-md mx-auto w-full h-screen flex justify-center items-center px-2">
             <div className="bg-white w-full sm:w-2/4 flex flex-col items-center border border-black rounded-xl gap-7 px-5 py-7">
-                <p className="font-bold text-xl border-b-2 border-[#FFB8B8] pb-2">Médico, faça o login!</p>
+            
+                <div className="w-full flex items-center justify-between">
+                    <Link
+                        href={"/"}
+                    >
+                        <FiArrowLeft className="w-8 h-fit"/>
+                    </Link>
+
+                    <p className="font-bold text-xl border-b-2 border-[#FFB8B8] pb-2">Médico, faça o login!</p>
+
+                    <div className="w-8"/>
+                </div>
 
                 <input 
                     type="number"
@@ -23,20 +37,20 @@ export default function LoginMedico() {
                 </button>
 
                 <div className="flex w-full justify-around items-center">
-                    <a 
+                    <Link 
                         href=""
                         className="border-b-2 border-[#FFB8B8] pb-2"
                     >
                         <p>Esqueceu a senha?</p>
-                    </a>
+                    </Link>
 
-                    <a 
+                    <Link 
                         href=""
                         className=" text-center border-b-2 border-[#FFB8B8] pb-2"
                     >
                         <p>Não possui conta?</p>
                         <p>Cadastre-se</p>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
