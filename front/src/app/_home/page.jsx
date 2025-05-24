9455b40787ab8639df7d156af129e1ba5f46f1aa
adriamweb <adriamestuda@gmail.com>
2025-05-24 20:35:59 -0300

    mudanças no style

--- front/src/app/_home/page.jsx
+++ front/src/app/_home/page.jsx
@@ -1,25 +1,33 @@
 import Link from "next/link";
 
 export default function HomePage() {
-    return (
-        <div className="max-w-screen-md mx-auto w-full h-screen flex justify-center items-center px-2">
-            <div className="bg-white w-full sm:w-2/4 flex flex-col items-center border border-black rounded-xl gap-5 py-7 px-5">
-                <p className="font-bold bg-[#FFB8B8] mb-8 p-2 rounded-2xl">Estou entrando como:</p>
+  return (
+    <div className="max-w-screen-md mx-auto w-full h-screen flex justify-center items-center px-2">
+      <div className="bg-white w-full sm:w-2/4 flex flex-col items-center rounded-xl gap-7 py-10 px-8 shadow-2xl text-xl">
+        <img
+          src="/Logo_SobreVidas.png"
+          alt="Logo ou imagem decorativa"
+          className="w-24 h-auto mb-4"
+        />
 
-                <Link 
-                    href={"/loginMedico"}
-                    className="w-32 text-center bg-[#FFB8B8] p-2 rounded-2xl"
-                >
-                    Médico(a)
-                </Link>
+        <p className="font-bold border-2 border-[#FFB8B8] mb-6 p-4 rounded-2xl ">
+          Estou entrando como:
+        </p>
 
-                <Link 
-                    href={""}
-                    className="w-32 text-center bg-[#FFB8B8] p-2 rounded-2xl"
-                >
-                    Enfermeiro(a)
-                </Link>
-            </div>
-        </div>
-    )
+        <Link
+          href={"/loginMedico"}
+          className="w-40 text-center p-3 rounded-2xl border-2 border-[#FFB8B8]"
+        >
+          Médico(a)
+        </Link>
+
+        <Link
+          href={""}
+          className="w-40 text-center p-3 rounded-2xl border-2 border-[#FFB8B8]"
+        >
+          Enfermeiro(a)
+        </Link>
+      </div>
+    </div>
+  );
 }
--- front/src/app/loginMedico/page.jsx
+++ front/src/app/loginMedico/page.jsx
@@ -4,7 +4,8 @@ import { FiArrowLeft } from "react-icons/fi";
 export default function LoginMedico() {
     return (
         <div className="max-w-screen-md mx-auto w-full h-screen flex justify-center items-center px-2">
-            <div className="bg-white w-full sm:w-2/4 flex flex-col items-center border border-black rounded-xl gap-7 px-5 py-7">
+            
+            <div className="bg-white w-full sm:w-2/4 flex flex-col items-center rounded-xl gap-7 px-5 py-7 shadow-2xl">
             
                 <div className="w-full flex items-center justify-between">
                     <Link
@@ -13,25 +14,31 @@ export default function LoginMedico() {
                         <FiArrowLeft className="w-8 h-fit"/>
                     </Link>
 
-                    <p className="font-bold text-xl border-b-2 border-[#FFB8B8] pb-2">Médico, faça o login!</p>
+                    <img 
+                        src="/Logo_SobreVidas.png" 
+                        alt="Logo ou imagem decorativa"
+                        className="w-24 h-auto mb-4"
+                    />
+
 
                     <div className="w-8"/>
                 </div>
 
+                    <p className="font-bold text-xl border-b-2 border-[#FFB8B8] pb-2">Médico, faça o login!</p>
                 <input 
                     type="number"
                     placeholder="CRM"
-                    className="bg-[#F4EEEE] py-2 px-2 rounded-md"
+                    className="bg-[#F4EEEE] py-2 px-2 rounded-md outline-none"
                 />
 
                 <input 
                     type="text"
                     placeholder="Senha"
-                    className="bg-[#F4EEEE] py-2 px-2 rounded-md"
+                    className="bg-[#F4EEEE] py-2 px-2 rounded-md outline-none"
                 />
 
                 <button 
-                    className="text-center bg-[#FFB8B8] px-4 py-2 rounded-2xl"
+                    className="text-center border-2 border-[#FFB8B8] px-4 py-2 rounded-2xl"
                 >
                     Entrar
                 </button>
@@ -46,9 +53,8 @@ export default function LoginMedico() {
 
                     <Link 
                         href=""
-                        className=" text-center border-b-2 border-[#FFB8B8] pb-2"
+                        className="border-b-2 border-[#FFB8B8] text-center pb-2"
                     >
-                        <p>Não possui conta?</p>
                         <p>Cadastre-se</p>
                     </Link>
                 </div>
