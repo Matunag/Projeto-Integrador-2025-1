562c1242449285484bc2def61861cf2b3f62d1d5
Fernando Brettas <fernandochbrettas@gmail.com>
2025-06-11 19:09:35 -0300

    Corrigir ponteiros do model

--- back/model/ubs.go
+++ back/model/ubs.go
@@ -1,11 +1,10 @@
 package model
 
-
 type Ubs struct {
-	ID         *int       `json:"id"`
-	EnderecoID *int       `json:"endereco_id"`
+	ID         *int      `json:"id"`
+	EnderecoID *int      `json:"endereco_id"`
 	Endereco   *Endereco `json:"endereco"`
-	Nome       *string    `json:"nome"`
-	CNES       *string    `json:"cnes"`
-	Prontuario *string    `json:"prontuario"`
+	Nome       *string   `json:"nome"`
+	CNES       *string   `json:"cnes"`
+	Prontuario *string   `json:"prontuario"`
 }
